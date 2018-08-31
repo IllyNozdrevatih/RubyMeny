9e9f0cd156a637f1fc8734aa22ecc0f9710c4938
Илья <IllyNozdrevatih>
2018-09-01 00:10:58 +0200

    добавил тесты на модели:dish,user

---
+++ spec/model/dish_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe Dish, :type => :model do
+  subject { 
+  	described_class.new(
+	  	name: "Катлета кивская", weight: "200",
+	  	  measure:"гр", price:15.2 , units: 0 ,
+        category_id:2
+  )}
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is valid without name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end 
+
+  it "is valid without weight" do 
+    subject.weight = nil
+    expect(subject).to_not be_valid
+  end 
+
+  it "is valid without measure" do 
+    subject.measure = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is valid without price" do 
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end  
+
+  it "is valid without units" do 
+    subject.units = nil
+    expect(subject).to be_valid
+  end 
+
+  it "is valid without category_id" do 
+    subject.category_id = nil
+    expect(subject).to_not be_valid
+  end   
+end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  subject { 
+  	described_class.new(
+	  	email: "valid@mail.ru", encrypted_password: "$2a$11$3J/wK6acuPuVQhwSZyqZQufPKmtrZ93MPSTNnzkz.3YG9UhaMaPn.",
+	  	created_at:DateTime.now , updated_at:DateTime.now
+  )}
+  
+  it "is valid without a email" do
+  	subject.email = nil
+  	expect(subject).to_not be_valid
+  end	
+  
+  it "is valid with valid encrypted_password" do
+  	subject.encrypted_password = nil
+  	expect(subject).to_not be_valid
+  end	
+
+  it "is valid with valid created_at" do
+  	subject.created_at = nil
+  	expect(subject).to_not be_valid
+  end	
+
+  it "is valid with valid updated_at" do
+  	subject.updated_at = nil
+  	expect(subject).to_not be_valid
+  end		
+end
